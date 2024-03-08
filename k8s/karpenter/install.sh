00f1c76fe7d73451517673d848895ac3fffe75bb
Sheriff Joe Excel <realexcel2020@gmail.com>
2024-03-08 18:10:10 +0100

    change

--- k8s/karpenter/install.sh
+++ k8s/karpenter/install.sh
@@ -21,14 +21,14 @@ curl -fsSL https://raw.githubusercontent.com/aws/karpenter-provider-aws/v"${KARP
   --parameter-overrides "ClusterName=${CLUSTER_NAME}"
 
 # Logout of helm registry to perform an unauthenticated pull against the public ECR
-helm registry logout public.ecr.aws
+# helm registry logout public.ecr.aws
 
-helm upgrade --install karpenter oci://public.ecr.aws/karpenter/karpenter --version "0.35.0" --namespace "kube-system" --create-namespace \
-  --set "settings.clusterName=ecommerce-project-cluster" \
-  --set "settings.interruptionQueue=ecommerce-project-cluster" \
-  --set controller.resources.requests.cpu=1 \
-  --set controller.resources.requests.memory=1Gi \
-  --set controller.resources.limits.cpu=1 \
-  --set controller.resources.limits.memory=1Gi \
-  --wait --debug
+# helm upgrade --install karpenter oci://public.ecr.aws/karpenter/karpenter --version "${KARPENTER_VERSION}" --namespace "${KARPENTER_NAMESPACE}" --create-namespace \
+#   --set "settings.clusterName=${CLUSTER_NAME}" \
+#   --set "settings.interruptionQueue=${CLUSTER_NAME}" \
+#   --set controller.resources.requests.cpu=1 \
+#   --set controller.resources.requests.memory=1Gi \
+#   --set controller.resources.limits.cpu=1 \
+#   --set controller.resources.limits.memory=1Gi \
+#   --wait --debug
 
